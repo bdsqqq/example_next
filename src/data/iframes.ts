e07b0422386216e1cd4b4da40999a3eee932c1c9
Igor Bedesqui <igorbedesqui@gmail.com>
2022-09-27 13:57:58 -0300

    feat: make examples

---
+++ src/data/iframes.ts
@@ -0,0 +1,16 @@
+const iframePages = [
+  {
+    url: "homepage",
+    author: "Some author",
+    src: "https://www.ibm.com",
+    text: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem enim dolor cum rem architecto tenetur fugiat maxime quaerat a doloremque deleniti, maiores minima doloribus voluptates sequi praesentium voluptas veniam repellendus.`,
+  },
+  {
+    url: "events",
+    author: "Other author",
+    src: "https://www.ibm.com/events",
+    text: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem enim dolor cum rem architecto tenetur fugiat maxime quaerat a doloremque deleniti, maiores minima doloribus voluptates sequi praesentium voluptas veniam repellendus.`,
+  },
+];
+
+export default iframePages;
